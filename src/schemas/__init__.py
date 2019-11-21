ce8fff736749cc45608606bbb94b4659a50e18f1
ts <ts@appkode.ru>
2019-11-21 13:19:41 +0200

    Add apispec default auth param

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,7 +1,12 @@
 from .message import MessageCreateRequestSchema
 from .message import MessageCreateResponseSchema
 
+APISPEC_DEFAULT_PARAMS = [
+    {'in': 'header', 'name': 'Authorization', 'schema': {'type': 'string'}, 'required': 'true'},
+]
+
 __all__ = [
+    'APISPEC_DEFAULT_PARAMS',
     'MessageCreateRequestSchema',
     'MessageCreateResponseSchema',
 ]
--- src/views/message.py
+++ src/views/message.py
@@ -5,13 +5,14 @@ from aiohttp_apispec import docs
 from aiohttp_apispec import request_schema
 from aiohttp_apispec import response_schema
 
-from schemas import MessageCreateRequestSchema
+from schemas import MessageCreateRequestSchema, APISPEC_DEFAULT_PARAMS
 from schemas import MessageCreateResponseSchema
 from utils.auth import access_token_required
 
 
 @docs(
     tags=['SMS'], summary='Запрос отправки сообщения', description='Описание запроса',
+    parameters=APISPEC_DEFAULT_PARAMS,
 )
 @request_schema(MessageCreateRequestSchema)
 @response_schema(MessageCreateResponseSchema)
