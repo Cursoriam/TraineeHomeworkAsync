3a5ff862a4a9cbf1424b1e95e4cd51471e596d83
Vladimir Tokarev <coaka2013@gmail.com>
2019-11-28 01:00:43 +0300

    add google speech-to-text integration

---
+++ src/integrations/google_speech_to_text.py
@@ -0,0 +1,49 @@
+from base64 import b64encode
+from json import loads
+
+from requests import post
+from requests.exceptions import ConnectionError
+
+from settings import URL
+from settings import API_KEY
+
+
+class GoogleSTT:
+    @classmethod
+    def transcript(cls, audio_file: bytes) -> str:
+        try:
+            req = loads(
+                post(URL, params=dict(key=API_KEY, ),
+                        json=dict(
+                        audio=dict(content=b64encode(audio_file).decode(), ),
+                        config=dict(languageCode='en-US', ),
+                         )
+                     ).text
+            )
+        except ConnectionError:
+            raise ConnectionError('Too many requests')
+        print(req)
+        try:
+            results = req['results']
+        except Exception:
+            raise Exception('No param \'results\'')
+
+        transcripted: str = ''
+
+        for result in results:
+            try:
+                alternatives = result['alternatives']
+            except Exception:
+                raise Exception('No param \'alternatives\'')
+
+            try:
+                alternative = alternatives[0]
+            except Exception:
+                raise Exception('There is no index 0 in dict')
+
+            try:
+                transcripted = transcripted+alternative['transcript']
+            except Exception:
+                raise Exception('No param \'transcripted\'')
+
+        return transcripted
